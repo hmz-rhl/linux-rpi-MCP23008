3d2ba42ec6ace743a2d03477000baa9b80052f0d
hmz-rhl <77698738+hmz-rhl@users.noreply.github.com>
2022-07-25 15:33:13 +0200

    Create example.c

---
+++ example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "expander_i2c.h"
+
+int main(){
+  // on déclare une instance qui pilotera l'expander d'adresse 0x20
+  expander_t* expander = expander_init(0x20);
+  //on met a 1 le pin 0 de l'expander 0x20
+  expander_setPinGPIO(expander, 0);
+  
+  // on libère la mémoire allouée
+  expander_closeAndFree(expander);
+  
+  return EXIT_SUCCESS;
+}
